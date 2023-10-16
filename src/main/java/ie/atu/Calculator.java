03d2ebd2f1becd3e8bc9f82098e84470449045ef
Chuka <g00414637@atu.ie>
2023-10-16 10:34:20 +0100

    Added a new subtract function

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args){
-        divide();
+        subtract();
     }
-    public static void divide()
+    public static void subtract()
     {
         System.out.println("Please put your first number :");
         Scanner inputs = new Scanner(System.in);
@@ -15,7 +15,7 @@ public class Calculator {
         System.out.println("Please put your second number :");
         int secondNumber = inputs.nextInt();
 
-        int total = firstNumber / secondNumber;
+        int total = firstNumber - secondNumber;
         System.out.println("The total is " + total);
     }
     public static void delete()
