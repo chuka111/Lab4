667cc2d48e51cd26e98660ad07e2cc7cae7f49bd
Chuka <g00414637@atu.ie>
2023-10-16 10:28:09 +0100

    Added a new multiply function

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args){
-        add();
+        multiply();
     }
-    public static void add()
+    public static void multiply()
     {
         System.out.println("Please put your first number :");
         Scanner inputs = new Scanner(System.in);
@@ -15,7 +15,7 @@ public class Calculator {
         System.out.println("Please put your second number :");
         int secondNumber = inputs.nextInt();
 
-        int total = firstNumber + secondNumber;
+        int total = firstNumber * secondNumber;
         System.out.println("The total is " + total);
     }
     public static void delete()
